f2a5b65c6daf92e2bd454f5eefedc84368993c94
Jonas Kirkemyr <jonas@kirkemyr.no>
2017-05-23 14:43:44 +0200

    chore: update demo build file

--- demo/script.ts
+++ demo/script.ts
@@ -1,11 +1,13 @@
 import * as Quill from 'quill';
-import {ImageResize} from '../src/ImageResize';
+import { ImageResize } from '../src/ImageResize';
 
 Quill.register('modules/imageResize', ImageResize);
 
 var quill = new Quill('#editor', {
 	theme: 'snow',
 	modules: {
-		imageResize: {}
+		imageResize: {
+			modules: ['Resize', 'DisplaySize', 'Toolbar']
+		}
 	}
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,23 @@
 var path = require('path');
 module.exports = {
-  entry: './demo/script.ts',
-  output: {
-    filename: './demo/script.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  }
-};
-/*
-module.exports = {
-    entry: "./src/ImageResize.js",
+    entry: './demo/script.ts',
     output: {
-        path: __dirname,
-        library: 'ImageResize',
-        libraryTarget: 'umd',
-        filename: "image-resize.min.js"
+        filename: './demo/script.js'
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                include: path.join(__dirname, 'src'),
-                exclude: /(node_modules|bower_components)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": [["es2015", { "modules": false }]],
-                        "plugins": ["babel-plugin-transform-class-properties"]
-                    }
-                }]
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
             },
             {
                 test: /\.svg$/,
-                use: [{
-                    loader: 'raw-loader'
-                }]
+                loader: 'svg-inline-loader'
             }
         ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
     }
-};*/
+};
